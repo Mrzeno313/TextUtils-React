3db8de4f399ca59976eae4bd47d5f258fa33df2c
Usama <63492230+Mrzeno313@users.noreply.github.com>
2022-10-05 12:14:50 +0500

    Updates

---
+++ precache-manifest.e591eeca7cb322819a7201733b8e88fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d63ed5c70e71cdc70d1",
+    "url": "/TextUtils-React/static/css/main.2833c5e5.chunk.css"
+  },
+  {
+    "revision": "3d63ed5c70e71cdc70d1",
+    "url": "/TextUtils-React/static/js/main.3d63ed5c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "0bff9c7d72544628105b02ef2b3c6888",
+    "url": "/TextUtils-React/index.html"
+  }
+];
